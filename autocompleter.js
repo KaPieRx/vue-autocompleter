a0f3c0e67a02e1a54657202b936036da05aa27a0
KaPieRx <kacperskocz@student.agh.edu.pl>
2021-05-23 21:04:25 +0200

    coś jest xD

--- autocompleter.js
+++ autocompleter.js
@@ -1,63 +1,99 @@
 Vue.component('v-autocompleter', {
+    template: `
+    <div class="autocom">
+        <div class="tekst">
+            <img class="lupa" src="lupa.png" @click="potwierdz()" title="Szukaj">
+            <input
+            :value="value"
+            type="text"
+            @input="$emit('input', $event.target.value)"
+            @keyup.enter="potwierdz()"
+            @keyup.down="goTo(podswietlony + 1)" 
+            @keyup.up="goTo(podswietlony - 1)"/>
+            <img class="klaw" src="klawiatura.png" title="Narzędzia do wprowadzania tekstu">
+            <img class="mikro" src="mikro.png" title="Wyszukiwanie głosowe">
+        </div>
+        <div class="result-city" v-for="(city, index) in filteredCities"
+        :class="{active : pokaz && podswietlony === index}" @click="potwierdz(city.name)">
+            <img class="lupa" src="lupa.png" title="Szukaj">
+            <span v-html="city.nameHtml"></span>
+        </div>
+    </div>`,
+
     data: function () {
         return {
-            googleSearch: "",
+            isOnResults: false,
+            googleSearch_temp: "",
             wyszukaj: "",
-            cities: window.cities,
-            pokaz: 0,
-            licznik: 0,
-        }
+            cities: window.cities.map((cityData) => {
+                cityData.nameLowerCase = cityData.name.toLowerCase();
+                return cityData;
+            }),
+            pokaz: false,
+            filteredCities: [],
+            podswietlony: 0,
+        };
     },
-    computed: {
-        filtredCities() {
-            let result = this.cities.filter(miasto => miasto.name.startsWith(this.googleSearch) || miasto.name.toLowerCase().startsWith(this.googleSearch));
-            if (this.googleSearch.length > 0) {
-                return result.slice(0, 10);
-            }
-        }
-    },
+
+    props: ['value', 'options'],
+
     watch: {
-        strzalki() {
-            if (googleSearch.length() > 0) {
-                document.addEventListener('keydown', this.wdol);
-                document.lista.addEventListener('keyup', this.wgore);
+        googleSearch_temp() {
+            if (this.pokaz) {
+                return;
             }
+            if (this.googleSearch_temp.length === 0) {
+                filteredCities = [];
+                return;
+            }
+            let returnedCities = [];
+            let searchLowerCase = this.googleSearch_temp.toLowerCase();
+
+            this.cities.forEach((cityData) => {
+                if (returnedCities.length === 10 || !cityData.nameLowerCase.includes(searchLowerCase)) {
+                    return;
+                }
+                returnedCities.push({
+                    name: cityData.name,
+                    nameHtml: cityData.nameLowerCase.replace(searchLowerCase, (match) => {
+                        return '<span class="bold">' + match + '</span>';
+                    })
+                })
+            });
+
+            this.filteredCities = returnedCities;
         }
     },
+
     methods: {
-        potwierdz() {
-
-            this.wyszukaj = this.googleSearch;
-            this.pokaz = 0;
-        },
-        uzupelnij(miasto) {
-
-            this.googleSearch = miasto.name;
-            this.wyszukaj = this.googleSearch;
-            this.pokaz = 0;
-        },
-        rozwin() {
-            this.pokaz = 1;
-        },
-        wdol() {
-            licznik++;
-            if (!licznik == lista.length) {
-                this.googleSearch = this.miasto[licznik].name;
-                // tu jakiś hover
+        goTo(index) {
+            if (!this.pokaz) {
+                index = 0;
             }
-        },
-        wgore() {
-            licznik--;
-            if (!licznik == 0) {
-                licznik++;
-                this.googleSearch = this.miasto[licznik].name;
-                // tu jakiś hover
-            }
-        },
-    }
-})
 
-var app = new Vue({
-    el: "#app",
-    props: ['options'] = filtredCities,
-})
+            if (index > this.filteredCities.length - 1) {
+                index = 0;
+            } else if (index < 0) {
+                index = this.filteredCities.length - 1;
+            }
+
+            this.pokaz = true;
+            this.podswietlony = index;
+            this.googleSearch_temp = this.filteredCities[index].name;
+        },
+        potwierdz(name) {
+            this.pokaz = true;
+
+            if (name) {
+                this.googleSearch_temp = name;
+            }
+
+            this.isOnResults = true;
+            this.filteredCities = [];
+            this.$nextTick(() => {
+                this.pokaz = false;
+            });
+        },
+    },
+});
+
